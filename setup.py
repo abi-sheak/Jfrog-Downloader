df2ef4d73f2583ddc619a2b81fa317efa90856b7
abi-sheak <abisheakk2@gmail.com>
2025-06-30 15:42:16 +0530

    Added pass in __init__ file

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Jfrog-Downloader',         # Required
-    version='1.0.0',                 # Required
+    name='Jfrog-Downloader', 
+    version='1.0.0',    
     description='Download files from JFrog Artifactory',
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     author='abi-sheak',
     author_email='abisheakkumarasamy@gmail.com',
     license='Apache License 2.0',
-    packages=find_packages(include=["Library", "Library/.*"]),
+    packages=find_packages(),
     install_requires=["requests"],
     python_requires=">=3.6",
 )
