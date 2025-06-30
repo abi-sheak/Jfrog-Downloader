16cacde1089923eccc66e1e681c2138915e4a022
mh0kh3 <abisheak.kumarasamy@aptiv.com>
2025-06-30 13:33:52 +0530

    Added init file in Library

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 from setuptools import setup, find_packages
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, "README.md"), "r", encoding="utf-8") as fd:
-    long_description = fd.read()
 
 setup(
-    name='jfrog-downloader',
-    version='1.0',
-    description='Downloading Images from Jfrog',
-    license="Apache License 2.0",
+    name='Jfrog-Downloader',         # Required
+    version='1.0.0',                 # Required
+    description='Download files from JFrog Artifactory',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     author='abi-sheak',
     author_email='abisheakkumarasamy@gmail.com',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=find_packages(include=["Library", "Library.*"]),
-    install_requires=["jfrgdownload", "abisheak"],
-    python_requires='>=3.6',
+    license='Apache License 2.0',
+    packages=find_packages(include=["Library", "Library/.*"]),
+    install_requires=["requests"],
+    python_requires=">=3.6",
 )
